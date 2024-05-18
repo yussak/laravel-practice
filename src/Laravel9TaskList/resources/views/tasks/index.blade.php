0bc0c52d5b199cfbeeabe802ec4545086132bcec
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-05-18 20:11:14 +0900

    task一覧の画面作成

---
+++ src/Laravel9TaskList/resources/views/tasks/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo App</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+    <nav class="my-navbar">
+        <a class="my-navbar-brand" href="/">ToDo App</a>
+    </nav>
+</header>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-4">
+                <nav class="panel panel-default">
+                    <div class="panel-heading">フォルダ</div>
+                    <div class="panel-body">
+                        <a href="#" class="btn btn-default btn-block">
+                            フォルダを追加する
+                        </a>
+                    </div>
+                    <div class="list-group">
+                        <table class="table foler-table">
+                            @foreach($folders as $folder)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item">
+                                        {{ $folder->title }}
+                                    </a>
+                                </td>
+                                <td><a href="#">編集</a></td>
+                                <td><a href="#">削除</a></td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </nav>
+            </div>
+            <div class="column col-md-8">
+            <!-- ここにタスクが表示される -->
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
