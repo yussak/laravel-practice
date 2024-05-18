eac0c8b2bb918a888dbf628a5b34462e7f89c023
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-05-18 20:11:05 +0900

    task一覧の処理追加

--- src/Laravel9TaskList/app/Http/Controllers/TaskController.php
+++ src/Laravel9TaskList/app/Http/Controllers/TaskController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
 
 class TaskController extends Controller
 {
-    // indexメソッドを追加
+    
+    /**
+     *  【タスク一覧ページの表示機能】
+     *
+     *  GET /folders/{id}/tasks
+     *  @param int $id
+     *  @return \Illuminate\View\View
+     */
     public function index()
     {
-        return "hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
